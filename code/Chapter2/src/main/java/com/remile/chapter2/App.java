52e14fbcc0e409a5f028ec5003ed31c1b83b4609
remilelei(雷博生) <remilelei@tencent.com>
2019-04-01 14:02:10 +0800

    建立第二章代码项目

---
+++ code/Chapter2/src/main/java/com/remile/chapter2/App.java
@@ -0,0 +1,13 @@
+package com.remile.chapter2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
