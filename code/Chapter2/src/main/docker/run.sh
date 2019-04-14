28db9e4ebc1f040cfc00db635a217cef41eeeefe
remilelei <m13349301365@163.com>
2019-04-14 22:22:21 +0800

    第二章 部署至docker，并配置docker-compose（文件补充）

---
+++ code/Chapter2/src/main/docker/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "********************************************************"
+echo "Starting remile test"
+echo "********************************************************"
+java -jar /usr/local/remile/@project.build.finalName@.jar
